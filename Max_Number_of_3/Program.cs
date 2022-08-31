3e816400f0297d31233fd7034d1ee72b2a99e324
Max <Max.Vinner@bk.ru>
2022-08-31 19:46:40 +0400

    make task3

---
+++ Max_Number_of_3/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("введите число а ");
+try
+{
+    int max = Int32.Parse(Console.ReadLine());
+    Console.Write("введите число b ");
+    int b = Int32.Parse(Console.ReadLine());
+    Console.Write("введите число c ");
+    int c = Int32.Parse(Console.ReadLine());
+    if (max < b)
+    {
+        max = b;
+    }
+    if (c > max)
+    {
+        max = c;
+    }
+    Console.WriteLine("max = "+max);
+}
+
+catch 
+{
+    Console.Write("число... ");
+}
