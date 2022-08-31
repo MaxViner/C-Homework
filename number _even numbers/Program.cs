8d1cdb4f0f247535d317b61fadcdddadc2f22d30
Max <Max.Vinner@bk.ru>
2022-08-31 20:02:11 +0400

    make even numbers

---
+++ number _even numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("введите число / выведем все четные числа от 1 до него ");
+try
+{   int i = 2;
+    int N = Int32.Parse(Console.ReadLine());
+    while (i <= N)
+    {
+        if (i % 2 == 0)
+        {
+        Console.Write(i+", ");
+        }
+        i++;
+    }
+}
+catch 
+{
+    Console.Write("число... ");
+}
