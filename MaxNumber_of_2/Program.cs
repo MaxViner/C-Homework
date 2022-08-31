8c7837ed12e9d1e412cf20a61786675ba9061bbe
Max <Max.Vinner@bk.ru>
2022-08-31 19:34:11 +0400

    make task2

---
+++ MaxNumber_of_2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("введите число а ");
+try
+{
+    int max = Int32.Parse(Console.ReadLine());
+    Console.Write("введите число b ");
+    int b = Int32.Parse(Console.ReadLine());
+    if (max > b)
+    {
+    Console.WriteLine("max = "+max);
+    }
+    else 
+    {
+    Console.WriteLine("max = "+b);
+    }
+}
+
+catch 
+{
+    Console.Write("число... ");
+}
