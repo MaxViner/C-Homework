7acbd716387d1cc0e853f4bfd30151fb23cb0cc2
Max <Max.Vinner@bk.ru>
2022-08-31 20:02:43 +0400

    make task6

---
+++ parity/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("введите число / посмотрим четное ли оно ");
+try
+{
+    int number = Int32.Parse(Console.ReadLine());
+    if (number % 2 == 0)
+    {
+        Console.Write("четное! ");
+    }
+    else
+    {
+        Console.Write("не четное ");
+    }
+}
+catch 
+{
+    Console.Write("число... ");
+}
